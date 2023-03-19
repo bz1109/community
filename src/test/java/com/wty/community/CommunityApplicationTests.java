a92495e98184c70deef109523eaf925623b794b0
bz1109 <45236179+bz1109@users.noreply.github.com>
2023-03-19 13:21:50 +0800

    Update CommunityApplicationTests.java

--- src/test/java/com/wty/community/CommunityApplicationTests.java
+++ src/test/java/com/wty/community/CommunityApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+// 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
 class CommunityApplicationTests implements ApplicationContextAware {
